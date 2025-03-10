42142a1a18cab95957ee0735669273e9afdcfc33
Mahesh Sherekar <maheshsherekar@hkbdevelopment.com>
2025-03-10 17:59:52 +0530

    Filtering related functaionality

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,39 +1,131 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Filter.module.css";
 import Data from "../data/Data";
 const Filter = ({data})=>{
-
-    const check = (e)=>{
-        var t = e.target.value;
+    const [existType, setExistType] = useState("");
+    const search = (e)=>{
+        var value = e.target.value;
         const searchedData = Data.filter((val,index)=>{
             let companyName = val.companyName.toString();
             let productName = val.productName.toString();
             let color = val.color.toString();
             let price = val.price.toString();
             let type = val.type.toString();
-            let result = companyName.match(new RegExp(t, "i")) || productName.match(new RegExp(t, "i")) || color.match(new RegExp(t, "i")) || price.match(new RegExp(t, "i")) || type.match(new RegExp(t, "i"));
+            let result = companyName.match(new RegExp(value, "i")) || productName.match(new RegExp(value, "i")) ||
+                color.match(new RegExp(value, "i")) || price == value || type.match(new RegExp(value, "i"));
             return result;
         })
-        data.setSeacrhProduct([searchedData]);
+        data.setSeacrhProduct(searchedData);
     }
+
+    const searchType = (e,status)=>{
+        let value = e.target.value;
+        let searchedData;
+  
+        if(data.seacrhProduct.length <=0 ){
+            searchedData = Data.filter((val)=>{
+                let result;
+                if(status != "prise"){
+                    let companyName = val.companyName.toString();
+                    let productName = val.productName.toString();
+                    let color = val.color.toString();
+                    let type = val.type.toString();
+                    result = companyName.match(new RegExp(value, "i")) || productName.match(new RegExp(value, "i")) || color.match(new RegExp(value, "i")) || type.match(new RegExp(value, "i"));
+                }
+                else{
+                    let prise = val.price;
+                    if(value == "0"){
+                        result =  prise >= value && prise < 1000;
+                    }else if(value == "1000"){
+                        result =  prise >= value && prise < 10000;
+                    }else{
+                        result =  prise >= value;
+                    }
+                }
+                return result;
+            })
+            data.setSeacrhProduct(searchedData);
+            setExistType(searchedData);
+            return;
+        };
+
+        if(status == "type"){
+            searchedData = Data.filter((val,index)=>{
+                let type = val.type.toString();
+                let result = type.match(new RegExp(value, "i"));
+                return result;
+            })
+
+            data.seacrhProduct.filter((val,id)=>{
+                existType == val.type ? val.type = searchedData : data.setSeacrhProduct(searchedData);
+            })
+            setExistType(searchedData)
+        }
+        else if(status == "company"){
+            searchedData = Data.filter((val,index)=>{
+                let companyName = val.companyName.toString();
+                let result = companyName.match(new RegExp(value, "i"));
+                return result;
+            })
+
+            data.seacrhProduct.filter((val,id)=>{
+                existType == val.type ? val.companyName = searchedData : data.setSeacrhProduct(searchedData);
+            })
+            setExistType(searchedData)
+        }
+        else if(status == "color"){
+            searchedData = Data.filter((val,index)=>{
+                let color = val.color.toString();
+                let result = color.match(new RegExp(value, "i"));
+                return result;
+            })
+
+            data.seacrhProduct.filter((val,id)=>{
+                existType == val.type ? val.color = searchedData : data.setSeacrhProduct(searchedData);
+            })
+            setExistType(searchedData)
+        } 
+        else if(status == "prise"){
+            searchedData = Data.filter((val,index)=>{
+                let prise = val.price;
+                let result;
+                if(value == "0"){
+                    result =  prise >= value && prise < 1000;
+                }else if(value == "1000"){
+                    result =  prise >= value && prise < 10000;
+                }else{
+                    result =  prise >= value;
+                }
+                
+                return result;
+            })
+
+            data.seacrhProduct.filter((val,id)=>{
+                existType == val.price ? val.price = searchedData : data.setSeacrhProduct(searchedData);
+            })
+            setExistType(searchedData)
+        }        
+        
+    }
+
     return(
         <>
             <div className={style.filter}>
-                <input type="text" placeholder="Search Product" onChange={(e)=>check(e)}/>
+                <input type="text" placeholder="Search Product" onChange={(e)=>search(e)}/>
                 <div className={style.filterOptions}>
                     <div className={style.buttons}>
                         <button onClick={()=>data.setIsList("list")}>List</button>
                         <button onClick={()=>data.setIsList("group")}>Group</button>
                     </div>
                     <div className={style.options}>
-                        <select>
+                        <select onChange={(e)=>searchType(e,"type")}>
                             <option>Headphone type</option>
                             <option>Featured</option>
                             <option>In-ear headphone</option>
                             <option>On-ear headphone</option>
                             <option>Over-ear headphone</option>
                         </select>
-                        <select>
+                        <select onChange={(e)=>searchType(e,"company")}>
                             <option>Company</option>
                             <option>Featured</option>
                             <option>JBL</option>
@@ -43,7 +135,7 @@ const Filter = ({data})=>{
                             <option>Marshall</option>
                             <option>Ptron</option>
                         </select>
-                        <select>
+                        <select onChange={(e)=>searchType(e,"color")}>
                             <option>Colour</option>
                             <option>Featured</option>
                             <option>Blue</option>
@@ -51,12 +143,12 @@ const Filter = ({data})=>{
                             <option>White</option>
                             <option>Brown</option>
                         </select>
-                        <select>
-                            <option>Price</option>
-                            <option>Featured</option>
-                            <option>₹0 - ₹1,000</option>
-                            <option>₹1,000 - ₹10,000</option>
-                            <option>₹10,000 - ₹20,000</option>
+                        <select onChange={(e)=>searchType(e,"prise")}>
+                            <option value="">Price</option>
+                            <option value="">Featured</option>
+                            <option value="0">₹0 - ₹1,000</option>
+                            <option value="1000">₹1,000 - ₹10,000</option>
+                            <option value="10000">₹10,000 - ₹20,000</option>
                         </select>
                     </div>
                     <div className={style.options}>
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -13,19 +13,34 @@ const Products = ()=>{
         <> 
             <div className={isList == "group" ? style.ContainerGroup : style.ContainerList}>
                 <Filter data={{isList, setIsList, seacrhProduct, setSeacrhProduct}}/>
-                
-                {Data.map((val,index)=>(
-                      <div className={isList == "group" ? style.boxGroup : style.boxList} key={index} onClick={()=>{navigate("/product/"+val.id)}}>
-                        <div className={style.boxImage}>
-                            <img src={val.asset} />
-                        </div>
-                        <div className={style.boxCaption}>
-                            <h4>{val.companyName} {val.productName}</h4>
-                            <h4>Price - {val.price}</h4>
-                            <h4>{val.color} {val.type}</h4>
-                        </div>
-                      </div>  
-                ))}
+                {seacrhProduct.length <= 0 &&
+                    Data.map((val,index)=>(
+                        <div className={isList == "group" ? style.boxGroup : style.boxList} key={index} onClick={()=>{navigate("/product/"+val.id)}}>
+                            <div className={style.boxImage}>
+                                <img src={val.asset} />
+                            </div>
+                            <div className={style.boxCaption}>
+                                <h4>{val.companyName} {val.productName}</h4>
+                                <h4>Price - {val.price}</h4>
+                                <h4>{val.color} {val.type}</h4>
+                            </div>
+                        </div>  
+                    ))
+                }
+                {seacrhProduct.length > 0 &&
+                    seacrhProduct.map((val,index)=>(
+                        <div className={isList == "group" ? style.boxGroup : style.boxList} key={index} onClick={()=>{navigate("/product/"+val.id)}}>
+                            <div className={style.boxImage}>
+                                <img src={val.asset} />
+                            </div>
+                            <div className={style.boxCaption}>
+                                <h4>{val.companyName} {val.productName}</h4>
+                                <h4>Price - {val.price}</h4>
+                                <h4>{val.color} {val.type}</h4>
+                            </div>
+                        </div>  
+                    ))
+                }
             </div>
         </>
     )
