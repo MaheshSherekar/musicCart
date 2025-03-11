592fef998cf215dada219c5fb2182a872043627e
Mahesh Sherekar <maheshsherekar@hkbdevelopment.com>
2025-03-11 18:00:36 +0530

    Filtering Data

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Filter.module.css";
 import Data from "../data/Data";
 const Filter = ({data})=>{
-    const [existType, setExistType] = useState("");
+    const [existType, setExistType] = useState({});
+
     const search = (e)=>{
         var value = e.target.value;
         const searchedData = Data.filter((val,index)=>{
@@ -20,94 +21,53 @@ const Filter = ({data})=>{
 
     const searchType = (e,status)=>{
         let value = e.target.value;
-        let searchedData;
-  
-        if(data.seacrhProduct.length <=0 ){
-            searchedData = Data.filter((val)=>{
-                let result;
-                if(status != "prise"){
-                    let companyName = val.companyName.toString();
-                    let productName = val.productName.toString();
-                    let color = val.color.toString();
-                    let type = val.type.toString();
-                    result = companyName.match(new RegExp(value, "i")) || productName.match(new RegExp(value, "i")) || color.match(new RegExp(value, "i")) || type.match(new RegExp(value, "i"));
-                }
-                else{
-                    let prise = val.price;
-                    if(value == "0"){
-                        result =  prise >= value && prise < 1000;
-                    }else if(value == "1000"){
-                        result =  prise >= value && prise < 10000;
-                    }else{
-                        result =  prise >= value;
-                    }
-                }
-                return result;
-            })
-            data.setSeacrhProduct(searchedData);
-            setExistType(searchedData);
-            return;
-        };
-
-        if(status == "type"){
-            searchedData = Data.filter((val,index)=>{
-                let type = val.type.toString();
-                let result = type.match(new RegExp(value, "i"));
-                return result;
-            })
-
-            data.seacrhProduct.filter((val,id)=>{
-                existType == val.type ? val.type = searchedData : data.setSeacrhProduct(searchedData);
-            })
-            setExistType(searchedData)
-        }
-        else if(status == "company"){
-            searchedData = Data.filter((val,index)=>{
-                let companyName = val.companyName.toString();
-                let result = companyName.match(new RegExp(value, "i"));
-                return result;
-            })
-
-            data.seacrhProduct.filter((val,id)=>{
-                existType == val.type ? val.companyName = searchedData : data.setSeacrhProduct(searchedData);
-            })
-            setExistType(searchedData)
-        }
-        else if(status == "color"){
-            searchedData = Data.filter((val,index)=>{
-                let color = val.color.toString();
-                let result = color.match(new RegExp(value, "i"));
-                return result;
-            })
-
-            data.seacrhProduct.filter((val,id)=>{
-                existType == val.type ? val.color = searchedData : data.setSeacrhProduct(searchedData);
-            })
-            setExistType(searchedData)
-        } 
-        else if(status == "prise"){
-            searchedData = Data.filter((val,index)=>{
-                let prise = val.price;
-                let result;
-                if(value == "0"){
-                    result =  prise >= value && prise < 1000;
-                }else if(value == "1000"){
-                    result =  prise >= value && prise < 10000;
-                }else{
-                    result =  prise >= value;
-                }
-                
-                return result;
-            })
-
-            data.seacrhProduct.filter((val,id)=>{
-                existType == val.price ? val.price = searchedData : data.setSeacrhProduct(searchedData);
-            })
-            setExistType(searchedData)
-        }        
-        
+        setExistType((prev)=>({...prev, [status] : value}));          
     }
 
+    useEffect(()=>{
+        const searchedData = Data.filter((val,index)=>{
+            let companyName = val.companyName.toString();
+            let color = val.color.toString();
+            let type = val.type.toString();
+            let price = val.price;
+            let result;
+            
+            let checkPrice = existType.price === "0" && (price >= 0 && price < 1000) || existType.price === "1000" && (price >= 1000 && price < 10000)
+            || existType.price === "10000" && (price >= 10000) ;
+
+            result =  checkPrice || existType.company  && (companyName == existType.company) || existType.color  && (color == existType.color) || existType.type  && (type == existType.type);
+           
+            return result;
+        })
+        data.setSeacrhProduct(searchedData);
+    },[existType]);
+
+    const searchSort = (e)=>{
+        let value = e.target.value;
+        let result;
+        if(value == "Lowest"){
+            result = Data.sort((a,b)=>{
+                return a.price - b.price;
+            })
+        }
+        else if(value == "Highest"){
+            result = Data.sort((a,b)=>{
+                return b.price - a.price;
+            })
+        }
+        else if(value == "A-Z"){
+            result = Data.sort((a,b)=>{
+                return a.companyName.localeCompare(b.companyName);
+            })
+        }
+        else if(value == "Z-A"){
+            result = Data.sort((a,b)=>{
+                return b.companyName.localeCompare(a.companyName);
+            })
+        }
+        data.setSeacrhProduct(result);
+    }
+    
     return(
         <>
             <div className={style.filter}>
@@ -143,7 +103,7 @@ const Filter = ({data})=>{
                             <option>White</option>
                             <option>Brown</option>
                         </select>
-                        <select onChange={(e)=>searchType(e,"prise")}>
+                        <select onChange={(e)=>searchType(e,"price")}>
                             <option value="">Price</option>
                             <option value="">Featured</option>
                             <option value="0">₹0 - ₹1,000</option>
@@ -152,13 +112,13 @@ const Filter = ({data})=>{
                         </select>
                     </div>
                     <div className={style.options}>
-                        <select>
+                        <select onChange={(e)=>searchSort(e)}>
                             <option>Sort by : Featured</option>
                             <option>Featured</option>
-                            <option>Price : Lowest</option>
-                            <option>Price : Highest</option>
-                            <option>Name : (A-Z)</option>
-                            <option>Name : (Z-A)</option>
+                            <option value="Lowest">Price : Lowest</option>
+                            <option value="Highest">Price : Highest</option>
+                            <option value="A-Z">Name : (A-Z)</option>
+                            <option value="Z-A">Name : (Z-A)</option>
                         </select>
                     </div>
                 </div>
